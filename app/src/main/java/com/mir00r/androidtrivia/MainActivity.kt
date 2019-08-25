455edd5ccc0acdfc92b7179452bcf77baaa68ce7
mir00r <abdur.razzaq06@gmail.com>
2019-08-25 23:44:55 +0600

    added sample fragments

--- app/src/main/java/com/mir00r/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/mir00r/androidtrivia/MainActivity.kt
@@ -2,11 +2,15 @@ package com.mir00r.androidtrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mir00r.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
