92643312bbfb809f8248017e589cca470fc6eba4
mir00r <abdur.razzaq06@gmail.com>
2019-08-26 00:25:55 +0600

    added title fragment and related XML layout and dimen resources values

--- app/src/main/java/com/mir00r/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/mir00r/androidtrivia/TitleFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.mir00r.androidtrivia.databinding.FragmentTitleBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,9 +19,11 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title, container, false
+        )
+        return binding.root
     }
 
 
