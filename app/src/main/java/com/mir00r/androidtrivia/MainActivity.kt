9bd6416c3919a2e7ed5c6a2d0096f0569dd54300
mir00r <abdur.razzaq06@gmail.com>
2019-08-26 16:46:35 +0600

    add upp button in the app bar of screen

--- app/src/main/java/com/mir00r/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/mir00r/androidtrivia/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mir00r.androidtrivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.mir00r.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,13 @@ class MainActivity : AppCompatActivity() {
         @Suppress("UNUSED_VARIABLE")
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
